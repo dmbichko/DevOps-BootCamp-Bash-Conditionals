9a3acfaf26693b5c3633efe43ffc42e3483d7d4b
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-09 09:06:10 +0300

    vet 1.0

--- check_string.sh
+++ check_string.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
-
-# Place your code here
+str=$1
+num=${#str}
+numbers=0
+symbols=0
+letters=0
+str_symbol="!@#$%^&()_+"
+for (( i = 0; i < $num; i++ ))
+{
+        if [[ ${str:$i:1} == [0-9] ]]; then
+                let "numbers = numbers + 1"
+        elif [[ ${str:$i:1} == [A-Za-z] ]]; then
+                let "letters = letters + 1"
+        elif [[ "$str_symbol" == *"${str:$i:1}"* ]]; then
+                let "symbols = symbols + 1"
+        fi
+}
+echo "Numbers: "$numbers "Symbols: "$symbols "Letters: "$letters
--- temp.sh
+++ temp.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-
-# Place your code here
+str=$1
+num=${#str}
+for (( i = 0; i < $num; i++ ))
+        {
+        if [[ ${str:$i:1} == "C" ]]; then
+                let "temp = ${str:0:$i} + 273"
+                echo $temp"K"
+                break
+        elif [[ ${str:$i:1} == "K" ]]; then
+                let "temp = ${str:0:$i} - 273"
+                echo $temp"C"
+                break
+        fi
+}
