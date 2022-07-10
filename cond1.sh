09429d59b23e7ef86b68f2ef6c87ca65b027c191
Dzmitry Bichko <dzmitry_bichko@epam.com>
2022-07-10 10:53:27 +0300

    cond1.sh

--- cond1.sh
+++ cond1.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 IFS=',' read -r -a array <<< "$1"
+sum=0
+for i in ${array[*]}; do
+         let ost=$i%2
+        [ $ost == 0 ] && let "sum+=$i"
+done
+echo $sum
 
 #Write your code here
